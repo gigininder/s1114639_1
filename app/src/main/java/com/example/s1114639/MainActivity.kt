b6da05d6fd5d3d78476473b32b5dc85f21f04753
gigininder <jshuang1225@gmail.com>
2024-05-22 00:47:49 +0800

    v2_黃俊翔 0522

--- app/src/main/java/com/example/s1114639/MainActivity.kt
+++ app/src/main/java/com/example/s1114639/MainActivity.kt
@@ -3,45 +3,111 @@ package com.example.s1114639
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.s1114639.ui.theme.S1114639Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             S1114639Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Greeting()
+                    Main()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun FirstScreen(navController: NavController) {
+
+    Column(modifier = Modifier
+
+    ) {
+        Text(text = "瑪利亞基金會服務總覽", color = Color.Blue)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    S1114639Theme {
-        Greeting("Android")
+fun SecondScreen(navController: NavController) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+
+    ) {
+        Text(text = "主要機構", color = Color.Red)
+
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Main() {
+    var showMenu by remember { mutableStateOf(false) }
+    val navController = rememberNavController()
+    Column {
+        TopAppBar(
+            title = {
+                Image(
+                    painter = painterResource(id = R.drawable.maria),
+                    contentDescription = "maria"
+                )
+            },
+            actions = {
+                IconButton(
+                    onClick = { showMenu = true }
+                ) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "More")
+                }
+                DropdownMenu(
+                    expanded = showMenu, onDismissRequest = { showMenu = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("簡介") },
+                        onClick = { navController.navigate("JumpFirst") })
+
+                    DropdownMenuItem(
+                        text = { Text("主要機構") },
+                        onClick = { navController.navigate("JumpSecond") })
+                }
+            }
+
+        )
+        NavHost(navController = navController, startDestination = "JumpFirst") {
+            composable("JumpFirst") {
+                FirstScreen(navController = navController)
+            }
+            composable("JumpSecond") {
+                SecondScreen(navController = navController)
+            }
+        }
     }
 }
